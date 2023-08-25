e55d049c15bb90bb0c32e729e644a8916b578432
Aman Patel <patelpatelaman00@gmail.com>
2023-08-25 08:38:49 +0530

    25

--- 25thAug/ArrayFunction.php
+++ 25thAug/ArrayFunction.php
@@ -1,7 +1,15 @@
-
 <?php
-echo"-------Flip array Function--------- <br><br>";
-$a = array("a" =>"red","b"=>"green","c"=>"blue","d"=>"black");
+echo "-------Flip array Function--------- <br><br>";
+$a = array("a" => "red", "b" => "green", "c" => "blue", "d" => "black");
 $result = array_flip($a);
 print_r($result);
+echo "<br><br>";
+
+echo "-------intersection array Function--------- <br><br>";
+$a1 = array("a" => "red", "b" => "green", "c" => "blue", "d" => "black");
+$a2 = array("e" => "blue", "f" => "green", "g" => "white");
+$a3 = array("red", "blue");
+$result1 = array_intersect($a1, $a2, $a3);
+print_r($result1);
+
 ?>
